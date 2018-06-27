b804168e366af1728f930a2d9a18bbd48fb1b51d
unknown <ahsan.mustafa.mughall@gmail.com>
2018-06-27 14:41:04 +0500

    initial commit

---
+++ src/app/customer/customer-orders.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { DataService } from '../core/services/data.service';
+import { ICustomer, IOrder, IOrderItem } from '../shared/interfaces';
+
+@Component({
+  selector: 'cm-customer-orders',
+  templateUrl: './customer-orders.component.html'
+})
+export class CustomerOrdersComponent implements OnInit {
+
+  orders: IOrder[] = [];
+  customer: ICustomer;
+
+  constructor(private route: ActivatedRoute, private dataService: DataService) { }
+
+  ngOnInit() {
+      this.route.parent.params.subscribe((params: Params) => {
+        const id = +params['id'];
+        this.dataService.getCustomer(id).subscribe((customer: ICustomer) => {
+          this.customer = customer;
+        });
+      });
+  }
+
+  ordersTrackBy(index: number, orderItem: any) {
+    return index;
+  }
+
+}
